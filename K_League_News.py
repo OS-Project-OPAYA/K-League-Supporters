b643945650eda8696ca4d3b757ace6278b909c13
Chominu <92563303+Chomw113@users.noreply.github.com>
2022-05-27 14:46:30 +0900

    Add files via upload

---
+++ K_League_News.py
@@ -0,0 +1,213 @@
+# ##kimchun
+# import requests
+# import requests.exceptions
+# import webbrowser
+# from bs4 import BeautifulSoup
+#
+# url = 'https://sports.news.naver.com/kfootball/club/postList?expertId=gimcheonfc'
+#
+# r = requests.get(url)
+# html = r.content
+# soup = BeautifulSoup(html, 'html.parser')
+# titles_html = soup.select('.news_list > ul > li > div > a > span')
+#
+# for i in range(len(titles_html)):
+#     print(i+1, titles_html[i].text)
+
+#-----------------------------------------------------------------------
+
+## ULSAN
+# import requests
+# import requests.exceptions
+# import webbrowser
+# from bs4 import BeautifulSoup
+#
+# url = 'https://sports.news.naver.com/kfootball/club/postList?expertId=253'
+#
+# r = requests.get(url)
+# html = r.content
+# soup = BeautifulSoup(html, 'html.parser')
+# titles_html = soup.select('.news_list > ul > li > div > a > span')
+#
+# for i in range(len(titles_html)):
+#     print(i+1, titles_html[i].text)
+
+#-----------------------------------------------------------------------
+
+#전북
+# import requests
+# import requests.exceptions
+# import webbrowser
+# from bs4 import BeautifulSoup
+#
+# url = 'https://sports.news.naver.com/kfootball/club/postList?expertId=256'
+#
+# r = requests.get(url)
+# html = r.content
+# soup = BeautifulSoup(html, 'html.parser')
+# titles_html = soup.select('.news_list > ul > li > div > a > span')
+#
+# for i in range(len(titles_html)):
+#     print(i+1, titles_html[i].text)
+
+#-----------------------------------------------------------------------
+
+#강원
+# import requests
+# import requests.exceptions
+# import webbrowser
+# from bs4 import BeautifulSoup
+#
+# url = 'https://sports.news.naver.com/kfootball/club/postList?expertId=315'
+#
+# r = requests.get(url)
+# html = r.content
+# soup = BeautifulSoup(html, 'html.parser')
+# titles_html = soup.select('.news_list > ul > li > div > a > span')
+#
+# for i in range(len(titles_html)):
+#     print(i+1, titles_html[i].text)
+
+#-----------------------------------------------------------------------
+
+#제주
+# import requests
+# import requests.exceptions
+# import webbrowser
+# from bs4 import BeautifulSoup
+#
+# url = 'https://sports.news.naver.com/kfootball/club/postList?expertId=257'
+#
+# r = requests.get(url)
+# html = r.content
+# soup = BeautifulSoup(html, 'html.parser')
+# titles_html = soup.select('.news_list > ul > li > div > a > span')
+#
+# for i in range(len(titles_html)):
+#     print(i+1, titles_html[i].text)
+
+#-----------------------------------------------------------------------
+
+# #서울
+# import requests
+# import requests.exceptions
+# import webbrowser
+# from bs4 import BeautifulSoup
+#
+# url = 'https://sports.news.naver.com/kfootball/club/postList?expertId=250'
+#
+# r = requests.get(url)
+# html = r.content
+# soup = BeautifulSoup(html, 'html.parser')
+# titles_html = soup.select('.news_list > ul > li > div > a > span')
+#
+# for i in range(len(titles_html)):
+#     print(i+1, titles_html[i].text)
+
+#-----------------------------------------------------------------------
+
+# #수원FC
+# import requests
+# import requests.exceptions
+# import webbrowser
+# from bs4 import BeautifulSoup
+#
+# url = 'https://sports.news.naver.com/kfootball/club/postList?expertId=542'
+#
+# r = requests.get(url)
+# html = r.content
+# soup = BeautifulSoup(html, 'html.parser')
+# titles_html = soup.select('.news_list > ul > li > div > a > span')
+#
+# for i in range(len(titles_html)):
+#     print(i+1, titles_html[i].text)
+
+#------------------------------------------------------------------------
+
+# #수원삼성
+# import requests
+# import requests.exceptions
+# import webbrowser
+# from bs4 import BeautifulSoup
+#
+# url = 'https://sports.news.naver.com/kfootball/club/postList?expertId=252'
+#
+# r = requests.get(url)
+# html = r.content
+# soup = BeautifulSoup(html, 'html.parser')
+# titles_html = soup.select('.news_list > ul > li > div > a > span')
+#
+# for i in range(len(titles_html)):
+#     print(i+1, titles_html[i].text)
+
+#-------------------------------------------------------------------------
+
+# #성남
+# import requests
+# import requests.exceptions
+# import webbrowser
+# from bs4 import BeautifulSoup
+#
+# url = 'https://sports.news.naver.com/kfootball/club/postList?expertId=521'
+#
+# r = requests.get(url)
+# html = r.content
+# soup = BeautifulSoup(html, 'html.parser')
+# titles_html = soup.select('.news_list > ul > li > div > a > span')
+#
+# for i in range(len(titles_html)):
+#     print(i+1, titles_html[i].text)
+
+#-----------------------------------------------------------------------
+
+# #대구
+# import requests
+# import requests.exceptions
+# import webbrowser
+# from bs4 import BeautifulSoup
+#
+# url = 'https://sports.news.naver.com/kfootball/club/postList?expertId=248'
+#
+# r = requests.get(url)
+# html = r.content
+# soup = BeautifulSoup(html, 'html.parser')
+# titles_html = soup.select('.news_list > ul > li > div > a > span')
+#
+# for i in range(len(titles_html)):
+#     print(i+1, titles_html[i].text)
+
+#-----------------------------------------------------------------------
+
+# #포항
+# import requests
+# import requests.exceptions
+# import webbrowser
+# from bs4 import BeautifulSoup
+#
+# url = 'https://sports.news.naver.com/kfootball/club/postList?expertId=258'
+#
+# r = requests.get(url)
+# html = r.content
+# soup = BeautifulSoup(html, 'html.parser')
+# titles_html = soup.select('.news_list > ul > li > div > a > span')
+#
+# for i in range(len(titles_html)):
+#     print(i+1, titles_html[i].text)
+
+#-----------------------------------------------------------------------
+
+# #인천
+# import requests
+# import requests.exceptions
+# import webbrowser
+# from bs4 import BeautifulSoup
+#
+# url = 'https://sports.news.naver.com/kfootball/club/postList?expertId=254'
+#
+# r = requests.get(url)
+# html = r.content
+# soup = BeautifulSoup(html, 'html.parser')
+# titles_html = soup.select('.news_list > ul > li > div > a > span')
+#
+# for i in range(len(titles_html)):
+#     print(i+1, titles_html[i].text)
